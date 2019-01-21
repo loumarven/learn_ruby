bd6f965afb8a5a1ac8f3b8e962f096861de1329f
Loumarven S. Payot <lou.payot@gmail.com>
2019-01-21 23:14:38 +0800

    Finish ex 04 and 05

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,27 @@
 #write your code here
+def translate(string)
+  words = string.split(" ")
+  translated = []
+
+  words.each do |word|
+    consonants_start = "" 
+    index = 0
+
+    loop do
+      break if word[index].match(/[aeiou]/)
+
+      consonants_start = consonants_start + word[index]
+      index = index + 1
+    end
+
+    if word.slice(index - 1, 2) == "qu"
+      consonants_start = consonants_start + "u"
+      index = index + 1
+    end
+ 
+    translated.push(word[index..-1] + consonants_start + "ay")
+  end
+
+  translated.join(" ")
+end
+
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,27 @@
 class Book
 # write your code here
+  def title
+    words = @title.split(" ")
+    little_words = ["the", "and", "a", "an", "in", "of"]
+
+    capitalized_words = words.map { |word| to_capital(word) }
+
+    capitalized_words[0] = capitalized_words[0].capitalize
+    @title = capitalized_words.join(" ")
+  end
+
+  def title=(title)
+    @title = title
+  end
+
+  private
+  def to_capital(word)
+    little_words = ["the", "and", "a", "an", "in", "of"]
+
+    if not little_words.index(word)
+      word.capitalize!
+    end
+
+    word
+  end
 end
