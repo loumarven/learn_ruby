8dc0b5259ced2e524c60a3723575b24768638cd4
Loumarven S. Payot <lou.payot@gmail.com>
2019-01-23 19:55:02 +0800

    Finish ex 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,22 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    t = Time.at(@seconds).strftime "%Z %H %M %S"
+    t = t.split(" ")
+
+    s = t[3]
+    m = t[2]
+
+    # subtract time zone val from hour val since total hour includes time zone
+    h = t[1].to_i - t[0][1..-1].to_i
+    h = (h < 10) ? '0' + h.to_s : h.to_s # prepend '0' if hour is 1 digit only
+
+    h + ':' + m + ':' + s
+  end
 end
