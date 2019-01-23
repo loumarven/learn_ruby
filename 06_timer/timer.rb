7021a079625e759eafd1e1fec658b2ed939783a7
Loumarven S. Payot <lou.payot@gmail.com>
2019-01-23 19:59:58 +0800

    Minor refactoring on ex 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -10,13 +10,13 @@ class Timer
     t = Time.at(@seconds).strftime "%Z %H %M %S"
     t = t.split(" ")
 
-    s = t[3]
-    m = t[2]
+    s = t[3].to_s
+    m = t[2].to_s
 
     # subtract time zone val from hour val since total hour includes time zone
     h = t[1].to_i - t[0][1..-1].to_i
     h = (h < 10) ? '0' + h.to_s : h.to_s # prepend '0' if hour is 1 digit only
 
-    h + ':' + m + ':' + s
+    "#{h}:#{m}:#{s}"
   end
 end
